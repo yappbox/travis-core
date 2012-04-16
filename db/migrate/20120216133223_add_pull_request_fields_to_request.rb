381a7fcb7a7481d3588c9d43e58f6ad0e681e9d2
Sven Fuchs <svenfuchs@artweb-design.de>
2012-04-16 16:33:54 +0200

    try not starting a transaction for the ddl change

--- db/migrate/20120216133223_add_pull_request_fields_to_request.rb
+++ db/migrate/20120216133223_add_pull_request_fields_to_request.rb
@@ -1,4 +1,8 @@
 class AddPullRequestFieldsToRequest < ActiveRecord::Migration
+  def ddl_transaction(&block)
+    block.call # do not start a transaction
+  end
+
   def change
     add_column :requests, :event_type, :string
     add_column :requests, :comments_url, :string
